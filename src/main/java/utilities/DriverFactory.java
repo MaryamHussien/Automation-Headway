a78977238d36d7a1201c3951367290781262f6c6
maryam.farag <maryam.farag@gizasystems.onmicrosoft.com>
2024-12-17 06:55:59 +0200

    Editing headless option

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class DriverFactory {
     static WebDriver driver;
 
@@ -15,12 +17,14 @@ public class DriverFactory {
             if (browserName.equalsIgnoreCase("chrome")){
                 System.out.println("Initializing chrome driver" + System.getProperty("os.name"));
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--headless=new");
+                //chromeOptions.addArguments("--no-sandbox");
+                //chromeOptions.addArguments("--disable-dev-shm-usage");
                 driver = new ChromeDriver(chromeOptions);
             }
             else if(browserName.equalsIgnoreCase("edge")){
                 EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless");
+                edgeOptions.addArguments("--headless=new");
                 System.out.println("Initializing edge driver" + System.getProperty("os.name"));
                 driver = new EdgeDriver(edgeOptions);
             }
@@ -35,6 +39,7 @@ public class DriverFactory {
                 driver = new EdgeDriver();
             }
         }
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         if(maximize){
             System.out.println("Maximizing Window, The window size is: "+ driver.manage().window().getSize());
             driver.manage().window().maximize();
